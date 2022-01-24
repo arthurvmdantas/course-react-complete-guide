5551d7f8b4e77bd996a662aa6b2c6346964dd11d
Arthur V. M. Dantas <me@arthurvmdantas.work>
2022-01-24 09:39:28 -0300

    lecture 40

--- src/components/Expenses.js
+++ src/components/Expenses.js
@@ -1,11 +1,12 @@
 import ExpenseItem from "./ExpenseItem";
+import Card from "./Card";
 import "./Expenses.css";
 
 function Expenses(props) {
   const expenses = props.expenses;
 
   return (
-    <div className="expenses">
+    <Card className="expenses">
       {/* <ExpenseItem
         title={expenses[0].title}
         amount={expenses[0].amount}
@@ -35,7 +36,7 @@ function Expenses(props) {
           date={expense.date}
         />
       ))}
-    </div>
+    </Card>
   );
 }
 
