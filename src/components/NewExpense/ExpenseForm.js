ebc5f9b90695a95c7c306587e6696bc7dcb7e1ed
Arthur V. M. Dantas <arthurvmdantas@gmail.com>
2022-02-14 08:42:30 -0300

    lecture 59

--- src/components/NewExpense/ExpenseForm.js
+++ src/components/NewExpense/ExpenseForm.js
@@ -1,7 +1,7 @@
 import { useState } from "react";
 import "./ExpenseForm.css";
 
-export default function ExpenseForm() {
+export default function ExpenseForm(props) {
   const [enteredTitle, setEnteredTitle] = useState("");
   const [enteredAmount, setEnteredAmount] = useState("");
   const [enteredDate, setEnteredDate] = useState("");
@@ -44,10 +44,10 @@ export default function ExpenseForm() {
       date: new Date(enteredDate),
     };
 
-    console.log(expenseData);
     setEnteredTitle("");
     setEnteredAmount("");
     setEnteredDate("");
+    props.onSaveExpenseData(expenseData);
   };
 
   return (
